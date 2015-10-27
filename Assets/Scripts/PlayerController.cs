e70bfd467552529a56792ea057b724a4b1cc0fe6
Henry Chau <henrychau123@gmail.com>
2015-10-26 20:55:50 -0500

    Added Movement (Up, Down, Left, Right)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
 public class PlayerController : MonoBehaviour {
 	
 	public int healAmount = 10;
 	public float speed;
-
+    public float tilt;
+    public Boundary bound;
 	private Rigidbody rb;
 	public PlayerHealth playerHealth;
 
@@ -19,9 +26,13 @@ public class PlayerController : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 		
-		Vector3 movement = new Vector3 (moveHorizontal,0.0f, moveVertical);
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
 		
-		rb.AddForce (movement * speed);
+		rb.velocity = (movement * speed);
+
+        rb.position = new Vector3(Mathf.Clamp(rb.position.x, bound.xMin, bound.xMax), Mathf.Clamp(rb.position.y, bound.yMin, bound.yMax), rb.position.z);
+
+        rb.rotation = Quaternion.Euler(rb.velocity.y * -tilt, 0.0f, rb.velocity.x * -tilt);
 	}
 	
 	void OnTriggerEnter(Collider other) 
