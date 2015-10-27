08056c573f109847892dad8e26f67e1df27e5e54
Henry Chau <henrychau123@gmail.com>
2015-10-27 01:21:04 -0500

    Add MotionParticleManager
    Randomly generated tiny particles that fly toward the camera, simulating
    forward motion

---
+++ Assets/Scripts/TinyMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TinyMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    public int speed;
+
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();	
+	}
+	
+	void FixedUpdate ()
+    {
+        Vector3 movement = new Vector3(0.0f, 0.0f, -1.0f);
+        rb.velocity = movement * speed;
+	}
+}
