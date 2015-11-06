0405fb567a0c3ae616be5c55284605c3669bcb5a
Erphun Ranjbar <emranjbar@uh.edu>
2015-11-06 17:13:21 -0600

    Added an intro warp drive as well as some background environment such as nebula cloud

---
+++ Assets/ParticleSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleSpawner : MonoBehaviour {
+
+	public GameObject[] environmentsToSpawn;
+	public float maxSpawnTime = 3f;            // How long between each spawn.
+	public Transform spawnPoint;         //spawn point the particles can spawn from.
+	// Use this for initialization
+	void Start () 
+	{
+		InvokeRepeating ("Spawn", 15f, maxSpawnTime);
+	}
+	
+	// Update is called once per frame
+	void Spawn()
+	{
+		int objectIndex = Random.Range (0, environmentsToSpawn.Length);
+		Debug.Log (objectIndex);
+		Instantiate (environmentsToSpawn[objectIndex], spawnPoint.position, spawnPoint.rotation);
+	}
+}
