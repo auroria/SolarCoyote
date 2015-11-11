3fbb7a0765c1df2f7693d1c40d6336015e252a27
Justin Nguyen <justin.nguyen01@gmail.com>
2015-11-10 21:07:06 -0600

    Added crosshair
    Player >> Crosshair >> change the texture2D to whatever you want
    crosshair to be
    Crosshair I shittily made is under:
    Textures >> xhair
    
    made 2 bullet shooters, increased bullet speed

---
+++ Assets/Scripts/ShowCrosshair.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowCrosshair : MonoBehaviour {
+	public Texture2D cursorImage;
+	public int cursorWidth = 32;
+	public int cursorHeight = 32;
+	void Start()
+	{
+		Cursor.visible = false;
+	}
+	void OnGUI()
+	{
+		GUI.DrawTexture(new Rect(Input.mousePosition.x, Screen.height - Input.mousePosition.y, cursorWidth, cursorHeight), cursorImage);
+	}
+}
