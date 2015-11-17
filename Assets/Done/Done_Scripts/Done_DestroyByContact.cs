7e544b0aea5612519ddd135b5d5eb2401a04ebab
elodi <lien_eat_godzilla@hotmail.com>
2015-11-16 18:17:59 -0600

    updated the "Done" Space shooter
    added hp and pickups

--- Assets/Done/Done_Scripts/Done_DestroyByContact.cs
+++ Assets/Done/Done_Scripts/Done_DestroyByContact.cs
@@ -7,7 +7,9 @@ public class Done_DestroyByContact : MonoBehaviour
 	public GameObject playerExplosion;
 	public int scoreValue;
 	private Done_GameController gameController;
-	
+	private Done_HealthBar hb;
+	public GameObject healthPickUp;
+
 	void Start ()
 	{
 		GameObject gameControllerObject = GameObject.FindGameObjectWithTag ("GameController");
@@ -20,7 +22,8 @@ public class Done_DestroyByContact : MonoBehaviour
 			Debug.Log ("Cannot find 'GameController' script");
 		}
 	}
-	
+
+
 	void OnTriggerEnter (Collider other)
 	{
 		if (other.tag == "Boundary" || other.tag == "Enemy")
@@ -31,16 +34,32 @@ public class Done_DestroyByContact : MonoBehaviour
 		if (explosion != null)
 		{
 			Instantiate(explosion, transform.position, transform.rotation);
+			//Destroy (other.gameObject);
+			Destroy (gameObject);
 		}
+
 		
-		if (other.tag == "Player")
-		{
-			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
-			gameController.GameOver();
+		if (other.tag == "Player") {
+			hb = other.GetComponent<Done_HealthBar> ();
+			//if player collides with health pick up
+			if (this.tag == "Health Pick Up") {
+				hb.setPlayerHealth (hb.getPlayerHealth () + 10);
+				hb.SetCurrentHealth (hb.getPlayerHealth ());
+			}else{ //player collides with hazard
+				hb.setPlayerHealth (hb.getPlayerHealth () - 10);
+				hb.SetCurrentHealth (hb.getPlayerHealth ());
+			}
+			//check if game is over
+			if (hb.getPlayerHealth () < 0) {
+				Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+				gameController.GameOver ();
+				Destroy (other.gameObject);
+			}
+		} else {
+			gameController.AddScore (scoreValue);
+			Instantiate(healthPickUp, transform.position, transform.rotation);
 		}
-		
-		gameController.AddScore(scoreValue);
-		Destroy (other.gameObject);
-		Destroy (gameObject);
 	}
+
+
 }
---
+++ Assets/Done/Done_Scripts/Done_HealthBar.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Done_HealthBar : MonoBehaviour
+{
+	public Vector2 pos;
+	public Vector2 size;
+	
+	private Texture2D fullTex;
+	public int minHealth, maxHealth;
+	private float healthPercent;
+
+	private int playerHealth;
+	
+	void Start ()
+	{
+		fullTex = InitFullTex ();
+		playerHealth = 100;
+		SetCurrentHealth (playerHealth);
+	}
+
+	public int getPlayerHealth()
+	{
+		return playerHealth;
+	}
+	public void setPlayerHealth (int health)
+	{
+		playerHealth = health;
+	}
+
+	void OnGUI ()
+	{
+		float widthOfHealthBar = size.x * healthPercent;
+		GUI.BeginGroup (new Rect (pos.x, pos.y, widthOfHealthBar, size.y));
+		GUI.DrawTexture (new Rect (0, 0, widthOfHealthBar, size.y), fullTex, ScaleMode.StretchToFill);
+		GUI.EndGroup ();
+	}
+	
+	public void SetMinMaxHealth (int minHealth, int maxHealth)
+	{
+		this.minHealth = minHealth;
+		this.maxHealth = maxHealth;
+	}
+	
+	public void SetCurrentHealth (int health)
+	{
+		healthPercent = health / (float)(maxHealth - minHealth);
+	}
+	
+	private Texture2D InitFullTex ()
+	{
+		
+		Texture2D tex = new Texture2D ((int)size.x, (int)size.y);
+		Color[] colors = tex.GetPixels ();
+		for (int i = 0; i < colors.Length; i++) {
+			//Fully red
+			colors [i] = Color.red;
+		}
+		
+		tex.SetPixels (colors);
+		tex.Apply ();
+		return tex;
+	}
+}
